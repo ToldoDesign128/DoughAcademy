481099c32994b693ea38177d3229db4f92983202
Matteo <matteobruschetti@gmail.com>
2021-11-19 16:25:07 +0100

    newsletter form

---
+++ page-newsletter.php
@@ -0,0 +1,14 @@
+<!------------------------------------------------------
+    Pagina per la form di Mailchimp
+------------------------------------------------------->
+<?php get_header(); ?>
+    
+    <div class="container newsletter pb-s pt-xl">
+        <div class="mb-xl">
+            <h2 class="mb-s">Newsletter</h2>
+            <p class="mb-xl">Iscriviti alla newsletter Dough Academy per ricevere tutti i nostri aggiornamenti in tempo reale.</p> 
+            <?php the_content(); ?>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
